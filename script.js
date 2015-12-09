7cd5565de1e595d2415463bf5551cb520a44305d
iamunknown2 <eugenelau@gmail.com>
2015-12-10 07:56:55 +0800

    Added 'I' to the exception list for badCase function

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ function exception(string, exceptionList)
 function badCases(string)
 {
 	var puncList = [".", "!", "?", "'", ",", "\"", "“", "”", ":", ";"];
-	var capsExceptions = ["AMA", "GTG", "BRB", "OK", "AFAIK", "AFAICT", "OTOH", "FTW", "LOL", "IIRC", "TTYL", "BTW", "FTFY", "IYTAI", "MERRY CHRISTMAS", "HAPPY HOLIDAYS"]; // You can get away with all-caps-ing these...
+	var capsExceptions = ["AMA", "GTG", "BRB", "OK", "AFAIK", "AFAICT", "OTOH", "FTW", "LOL", "IIRC", "TTYL", "BTW", "FTFY", "IYTAI", "I", "MERRY CHRISTMAS", "HAPPY HOLIDAYS"]; // You can get away with all-caps-ing these...
 	var necPuncList = [".", "!", "?"]; // You MUST put a capital after these punctuation marks.
 	var exceptString = exception(string, capsExceptions);
 	var newString = exceptString.replace(/ /g, "");
